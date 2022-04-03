39ae4eb8d9e8ee3e87e383a56fb0398bad409dbe
ganm0r <gndhrvmv@gmail.com>
2022-04-03 18:50:29 +0530

    :wrench: Use express error handling

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorhandler");
 
 const port = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalrouter"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`[server] listening on port ${port}...`));
