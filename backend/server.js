d8bc4a343c37c1c9a25d9806e75627c29bae59a5
ganm0r <gndhrvmv@gmail.com>
2022-04-03 18:26:58 +0530

    :construction: Add routes with controllers in index file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('[test] running nodemon with `npm run server`...');
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalrouter"));
+
+app.listen(port, () => console.log(`[server] listening on port ${port}...`));
