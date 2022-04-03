3e8f1246df3c6d2e4aa454102b9ccde09bd9a7e9
ganm0r <gndhrvmv@gmail.com>
2022-04-03 18:52:02 +0530

    :alien: Make module exports asynchronous

--- backend/controllers/goalcontroller.js
+++ backend/controllers/goalcontroller.js
@@ -1,38 +1,45 @@
+const asyncHandler = require("express-async-handler");
+
 /*
     @description    get goals
     @route          GET /api/goals
     @access         private
 */
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).send({ message: 'get goals' });
-};
+});
 
 /*
     @description    set goal
     @route          POST /api/goals
     @access         private
 */
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error("[error] please add a text field");
+    }
+
     res.status(200).send({ message: 'set goal' });
-};
+});
 
 /*
     @description    update goal for id
     @route          PUT /api/goals/:id
     @access         private
 */
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).send({ message: `update goal where id=${req.params.id}` });
-};
+});
 
 /*
     @description    delete goal for id
     @route          DELTE /api/goals/:id
     @access         private
 */
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).send({ message: `delete goal where id=${req.params.id}` });
-};
+});
 
 module.exports = {
     getGoals,
