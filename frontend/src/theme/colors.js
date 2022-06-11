351f861bb5d39b18d23b84ce1322c8a3f1293754
ganm0r <gndhrvmv@gmail.com>
2022-06-11 22:53:57 +0530

    :lipstick: Define color scheme

--- frontend/src/theme/colors.js
+++ frontend/src/theme/colors.js
@@ -1,12 +1,18 @@
 const colors = {
-    /** #040600 */
-    "black": "#040600",
+  /** #040600 */
+  "black": "#040600",
 
-    /** #E67613 */
-    "orange": "#E67613",
+  /** #E67613 */
+  "orange": "#E67613",
 
-    /** #225932 */
-    "green": "#225932",
+  /** #225932 */
+  "green": "#225932",
+
+  /** #FFFFFF */
+  "white": "#FFFFFF",
+
+  /** #C4C4C4 */
+  "grey": "#C4C4C4",
 };
-  
-  export default colors;
+
+export default colors;
