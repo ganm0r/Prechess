b8ccb804adbc74a50bf2874fb2465e2cc5c736bb
ganm0r <gndhrvmv@gmail.com>
2022-06-11 22:51:09 +0530

    :building_construction: Add Button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+import colors from "../theme/colors";
+import typography from "../theme/typography";
+
+const StyledButton = styled.button`
+    background-color: ${colors.orange};
+    font-weight: ${typography.fontWeights.black};
+    font-size: 24px;
+    box-sizing: border-box;
+    color: white;
+    letter-spacing: 0.15em;
+    text-transform: uppercase;
+    padding: 14px;
+    outline: none;
+    border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    margin-top: 7%;
+
+    &:active {
+        transform: scale(0.98);
+    }
+`;
+
+const Button = ({ children, onClick, disabled, cursor, to }) => {
+    return (
+        <StyledButton
+            onClick={onClick}
+            disabled={disabled}
+            style={{ cursor: cursor }}
+        >
+            {children}
+        </StyledButton>
+    );
+};
+
+export { Button };
