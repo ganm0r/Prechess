09357bfbb7928e105db8e2115fc708cc2b3f026d
ganm0r <gndhrvmv@gmail.com>
2022-06-11 22:56:16 +0530

    :construction: Build Login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+import colors from "../theme/colors";
+import typography from "../theme/typography";
+
+import { Card } from "../components/Card";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
+const Grid = styled.div`
+  display: grid;
+  position: relative;
+`;
+
+const SubHeading = styled.h1`
+  margin: 0%;
+  font-weight: ${typography.fontWeights.semibold};
+  font-family: ${typography.fonts.primary};
+  font-size: 36px;
+  color: ${colors.white};
+  position: relative;
+`;
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <React.Fragment>
+      <Grid
+        style={{
+            gridTemplateColumns: "0.9fr 0.7fr",
+            alignItems: "center",
+            justifyItems: "center",
+            height: "95%",
+        }}
+      >
+        <Card 
+          gridTemplateRows={"repeat(4, 0.68fr)"}
+        >
+          <SubHeading>
+            Hello again! Let's sign you in...
+          </SubHeading>
+          <Input
+            type={"text"}
+            title={"Email Address"}
+            placeholder={"carlsenmagnus@gmail.com"}
+            required={true}
+            info={"Your email associated with this account!"}
+          />
+          <Input
+            type={"password"}
+            title={"Password"}
+            placeholder={"IAmWorldChamp@2022"}
+            required={true}
+            info={"Your strong password please!"}
+          />
+          <Button children={"Yes, take me in!"} />
+        </Card>
+      </Grid>
+    </React.Fragment>
   );
 };
 
-export default Login;
+export { Login };
