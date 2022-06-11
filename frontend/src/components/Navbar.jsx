a2747c1041070f1c09eb52b1efbab44415e4a23c
ganm0r <gndhrvmv@gmail.com>
2022-06-11 22:52:10 +0530

    :building_construction: Add Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../theme/colors";
+import typography from "../theme/typography";
+
+import { useNavigate } from "react-router-dom";
+
+const StyledNavbar = styled.div`
+    display: flex;
+    align-items: center;
+    max-width: 100vw;
+    background-color: ${colors.black};
+    padding: 8px 36px;
+    box-shadow: 0px 4px 8px ${colors.green};
+`;
+
+const SubHeading = styled.h1`
+  margin: 0;
+  font-weight: ${typography.fontWeights.black};
+  font-size: 36px;
+  line-height: 48px;
+  color: ${colors.white};
+  position: relative;
+  cursor: pointer;
+  role: button;
+`;
+
+const Navbar = () => {
+    const navigate = useNavigate();
+
+    return (
+        <React.Fragment>
+            <StyledNavbar>
+                <SubHeading onClick={() => navigate("/")}>PreChess</SubHeading>
+            </StyledNavbar>
+        </React.Fragment>
+    );
+};
+
+export { Navbar };
