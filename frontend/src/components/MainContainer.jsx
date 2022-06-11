aeeba2168aa1000537d8a976fa0a6d1e5a50d94b
ganm0r <gndhrvmv@gmail.com>
2022-06-11 22:58:10 +0530

    :construction_worker: Add main container

--- frontend/src/components/MainContainer.jsx
+++ frontend/src/components/MainContainer.jsx
@@ -1,12 +1,9 @@
 import styled from "styled-components";
 import colors from "../theme/colors";
 
-const MainContainer = styled.div(
-  ({ theme }) => `
-    width: 100vw;
-    height: 100vh;
-    background-color: ${colors["black"]};
-  `
-);
-
+const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${colors.black};
+`
 export { MainContainer };
