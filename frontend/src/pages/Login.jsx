d1e8d365031a4654eaf8ceaf943da052606092b4
ganm0r <gndhrvmv@gmail.com>
2022-06-12 19:52:32 +0530

    :construction: Add form functionality to Login and Register pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,6 +6,7 @@ import typography from "../theme/typography";
 import { Card } from "../components/Card";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState, useEffect } from "react";
 
 const Grid = styled.div`
   display: grid;
@@ -22,7 +23,31 @@ const SubHeading = styled.h1`
   user-select: none;
 `;
 
+const Form = styled.form`
+  display: grid;
+  position: relative;
+  grid-template-rows: repeat(3, 0.5fr);
+`;
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <React.Fragment>
       <Grid
@@ -33,27 +58,44 @@ const Login = () => {
             height: "96%",
         }}
       >
-        <Card 
-          gridTemplateRows={"repeat(4, 0.5fr)"}
-        >
-          <SubHeading>
-            Hello again! Let's sign you in...
-          </SubHeading>
-          <Input
-            type={"email"}
-            title={"Email Address"}
-            placeholder={"carlsenmagnus@gmail.com"}
-            required={true}
-            info={"Your email associated with this account!"}
-          />
-          <Input
-            type={"password"}
-            title={"Password"}
-            placeholder={"IAmWorldChamp@2022"}
-            required={true}
-            info={"Your strong password please!"}
-          />
-          <Button children={"Yes, take me in!"} />
+        <Card>
+          <Grid
+            style={{
+              gridTemplateRows: "1fr 5fr",
+            }}
+          >
+            <SubHeading>
+              Hello again! Let's sign you in 🚀
+            </SubHeading>
+            <Form onSubmit={onSubmit} >
+              <Input
+                type={"email"}
+                title={"Email Address"}
+                placeholder={"carlsenmagnus@gmail.com"}
+                required={true}
+                info={"Your email associated with this account!"}
+                id={"email"}
+                name={"email"}
+                value={email}
+                onChange={onChange}
+              />
+              <Input
+                type={"password"}
+                title={"Password"}
+                placeholder={"IAmWorldChamp@2022"}
+                required={true}
+                info={"Your strong password please!"}
+                id={"password"}
+                name={"password"}
+                value={password}
+                onChange={onChange}
+              />
+              <Button 
+                children={"Yes, take me in!"} 
+                type={"submit"}
+              />
+            </Form>
+          </Grid>
         </Card>
         <img
                 src="/prechess.jpg"
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -6,6 +6,7 @@ import typography from "../theme/typography";
 import { Card } from "../components/Card";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState, useEffect } from "react";
 
 const Grid = styled.div`
   display: grid;
@@ -22,7 +23,33 @@ const SubHeading = styled.h1`
   user-select: none;
 `;
 
+const Form = styled.form`
+  display: grid;
+  position: relative;
+  grid-template-rows: repeat(5, 0.5fr);
+`;
+
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const { username, email, password, confirmPassword } = formData;
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <React.Fragment>
       <Grid
@@ -33,41 +60,66 @@ const Register = () => {
             height: "96%",
         }}
       >
-        <Card 
-          gridTemplateRows={"repeat(6, 0.5fr)"}
-        >
-          <SubHeading>
-            Welcome! Let's sign you up...
-          </SubHeading>
-          <Input
-            type={"text"}
-            title={"Username"}
-            placeholder={"Magunus Carlsen"}
-            required={true}
-            info={"Your username associated with this account!"}
-          />
-          <Input
-            type={"email"}
-            title={"Email Address"}
-            placeholder={"carlsenmagnus@gmail.com"}
-            required={true}
-            info={"Your email associated with this account!"}
-          />
-          <Input
-            type={"password"}
-            title={"Password"}
-            placeholder={"IAmWorldChamp@2022"}
-            required={true}
-            info={"Your strong password please!"}
-          />
-          <Input
-            type={"password"}
-            title={"Confirm Password"}
-            placeholder={"IAmWorldChamp@2022"}
-            required={true}
-            info={"Your strong password please!"}
-          />
-          <Button children={"Yes, take me in!"} />
+        <Card>
+          <Grid
+            style={{
+              gridTemplateRows: "1fr 10fr",
+            }}
+          >
+            <SubHeading>
+              Welcome! Let's sign you up 🚀
+            </SubHeading>
+            <Form onSubmit={onSubmit} >
+              <Input
+                type={"text"}
+                title={"Username"}
+                placeholder={"Magunus Carlsen"}
+                required={true}
+                info={"Your username associated with this account!"}
+                id={"username"}
+                name={"username"}
+                value={username}
+                onChange={onChange}
+              />
+              <Input
+                type={"email"}
+                title={"Email Address"}
+                placeholder={"carlsenmagnus@gmail.com"}
+                required={true}
+                info={"Your email associated with this account!"}
+                id={"email"}
+                name={"email"}
+                value={email}
+                onChange={onChange}
+              />
+              <Input
+                type={"password"}
+                title={"Password"}
+                placeholder={"IAmWorldChamp@2022"}
+                required={true}
+                info={"Your strong password please!"}
+                id={"password"}
+                name={"password"}
+                value={password}
+                onChange={onChange}
+              />
+              <Input
+                type={"password"}
+                title={"Confirm Password"}
+                placeholder={"IAmWorldChamp@2022"}
+                required={true}
+                info={"Your strong password please!"}
+                id={"confirmPassword"}
+                name={"confirmPassword"}
+                value={confirmPassword}
+                onChange={onChange}
+              />
+              <Button 
+                children={"Yes, take me in!"} 
+                type={"submit"}
+              />
+            </Form>
+          </Grid>
         </Card>
         <img
           src="/prechess.jpg"
