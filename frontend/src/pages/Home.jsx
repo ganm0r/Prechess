db4c9534023222557caced672bd7ac237f7aa5c0
ganm0r <gndhrvmv@gmail.com>
2022-06-12 23:12:39 +0530

    :construction: Build temporary Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import colors from "../theme/colors";
+import typography from "../theme/typography";
+
+const Grid = styled.div`
+  display: grid;
+`;
+
+const SubHeading = styled.h1`
+  margin: 0%;
+  font-weight: ${typography.fontWeights.semibold};
+  font-family: ${typography.fonts.primary};
+  font-size: 32px;
+  color: ${colors.white};
+  position: relative;
+  user-select: none;
+`;
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <React.Fragment>
+      <Grid
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <SubHeading>
+          Registered successfully MF!
+        </SubHeading>
+      </Grid>
+    </React.Fragment>
   );
 };
 
