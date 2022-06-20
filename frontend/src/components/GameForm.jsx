58a791cdf1d875189c9375519f0eae502cc717bb
ganm0r <gndhrvmv@gmail.com>
2022-06-20 11:02:21 +0530

    :construction: Build game form

--- frontend/src/components/GameForm.jsx
+++ frontend/src/components/GameForm.jsx
@@ -5,10 +5,12 @@ import typography from "../theme/typography";
 
 import { Input } from "./Input";
 import { Button } from "./Button";
+import { Select } from "./Select";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { HashLoader } from "react-spinners";
+import { GrClose } from "react-icons/gr";
 
 const Overlay = styled.div`
     position: fixed;
@@ -16,7 +18,7 @@ const Overlay = styled.div`
     right: 0;
     left: 0;
     bottom: 0;
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.5);
     z-index: 10;
 `;
 
@@ -25,31 +27,63 @@ const StyledModal = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    overflow-x: hidden;
-    overflow: scroll;
+    overflow: hidden;
     scrollbar-width: none;
     background-color: ${colors.green};
     border-radius: 8px;
-    min-width: 720px;
-    max-width: 500px;
-    max-height: 720px;
+    min-width: 524px;
+    max-width: 524px;
+    min-height: 620px;
+
+    ::-webkit-scrollbar {
+        width: 0;
+        height: 0;
+    }
 `;
 
 const StyledModalForm = styled.form`
-  display: grid;
-  position: relative;
-  grid-template-rows: repeat(3, 0.5fr) 3fr 0.5fr;
-  margin: 2%;
-  margin-top: 0;
+    display: grid;
+    position: relative;
+    grid-template-rows: repeat(5, 0.5fr);
+    margin: 2%;
+    margin-top: 0;
 `;
 
 const Flex = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
+    display: flex;
+    max-width: 506px;
+    min-width: 506px;
+    margin: 0;
+    margin-top: 2%;
+
+    ::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    }
 `;
 
-const GameForm = ({ isOpen, onClose }) => {
+const SubHeading = styled.h1`
+    margin: 0;
+    font-weight: ${typography.fontWeights.black};
+    color: ${colors.white};
+    font-size: 32px;
+    position: relative;
+    user-select: none;
+`;
+
+const GAME_TYPES = [
+    {
+      type: "Opening",
+    },
+    {
+      type: "Preparation",
+    },
+    {
+      type: "Game",
+    },
+];
+
+const GameForm = ({ isOpen, onClose, gameData, title }) => {
     const [formData, setFormData] = useState({
         name: "",
         type: "",
@@ -59,8 +93,6 @@ const GameForm = ({ isOpen, onClose }) => {
     const { name, type, game } = formData;
 
     const dispatch = useDispatch();
-
-    //const { game, isLoading, isSuccess, isError, message } = useSelector((state) => state.game);
 
     const onChange = (event) => {
         setFormData((prevState) => ({
@@ -79,19 +111,6 @@ const GameForm = ({ isOpen, onClose }) => {
         };
     }
 
-    // if(isLoading) {
-    //     return (
-    //       <Flex
-    //         style={{
-    //           alignItems: "center",
-    //           justifyContent: "center"
-    //         }}
-    //       >
-    //         <HashLoader color={colors.orange} />
-    //       </Flex>
-    //     )
-    //   }
-
     if(!isOpen) return null;
 
     return (
@@ -99,44 +118,73 @@ const GameForm = ({ isOpen, onClose }) => {
             <Overlay>
                 <StyledModal>
                     <StyledModalForm>
-                        <Button onClick={onClose} children={"Close"}/>
+                        <Flex
+                            style={{
+                                    backgroundColor: `${colors.orange}`,
+                                    justifyContent: "space-between",
+                                    alignItems: "center",
+                                    borderRadius: "8px",
+                                    marginBottom: "4%",
+                                }}
+                            >
+                            <SubHeading
+                                style={{
+                                    margin: "2%",
+                                    color: `${colors.black}`,
+                                    textTransform: "uppercase",
+                                }}
+                            >
+                                {title} ✨
+                            </SubHeading>
+                            <GrClose
+                                size={"32px"}
+                                style={{
+                                    margin: "2%",
+                                    color: `${colors.black}`,
+                                }}
+                                onClick={onClose}
+                                cursor="pointer"
+                            />
+                        </Flex>
                         <Input
                             type={"text"}
                             title={"Name"}
-                            placeholder={"The Evans Gambit"}
+                            placeholder={"London System"}
                             required={true}
-                            info={"Maximum 30 characters"}
+                            info={"Maximum 30 characters please!"}
                             id={"name"}
                             name={"name"}
                             value={name}
                             onChange={onChange}
                         />
+                        <Select
+                            title={"Type"}
+                            required={true}
+                            info={"Select game type!"}
+                            id={"type"}
+                            name={"type"}
+                            value={type}
+                            onChange={onChange}
+                        >
+                            {GAME_TYPES.map((game) => (
+                                <option key={game.type} value={game.type}>{game.type}</option>
+                            ))}
+                        </Select>
                         <Input
                             type={"text"}
-                            title={"Type"}
-                            placeholder={"The Evans Gambit"}
-                            required={true}
-                            info={"Maximum 30 characters"}
-                            id={"name"}
-                            name={"name"}
-                            value={name}
-                            onChange={onChange}
-                        />
-                        <Input
-                            type={"password"}
                             title={"PNG"}
-                            placeholder={"IAmWorldChamp@2022"}
+                            placeholder={"1. d4 d5 2. Nf3 Nf6 3. Bf4"}
                             required={true}
-                            info={"Your strong password please!"}
-                            id={"password"}
-                            name={"password"}
+                            info={"Your awesome png please!"}
+                            id={"png"}
+                            name={"png"}
                             value={game}
                             onChange={onChange}
                         />
                         <Button
                             type={"submit"}
                         >
-                            Test<span style={{ paddingLeft: "4px" }}>🪴</span>
+                            Okay, add it!<span style={{ paddingLeft: "4px" }}>🪴</span>
                         </Button>
                     </StyledModalForm>
                 </StyledModal>
