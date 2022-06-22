c0083c04f2a9936939d715c5781382309b3e4c46
ganm0r <gndhrvmv@gmail.com>
2022-06-22 12:54:17 +0530

    :construction: Build game card

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "../theme/colors";
+import typography from "../theme/typography";
+
+import { useState } from "react";
+import { GameModal } from "./GameModal";
 
 const StyledCard = styled.div`
     display: grid;
@@ -24,19 +28,69 @@ const StyledCard = styled.div`
     }
 `;
 
-const GameCard = ({ children, gridTemplateRows, gameTitle, onGameCardClick }) => {
-  return (
-    <React.Fragment>
-        <StyledCard
-            style={{
-                gridTemplateRows: gridTemplateRows,
-            }}
-            onClick={onGameCardClick}
-        >
-            {children}
-        </StyledCard>
-    </React.Fragment>
-  );
+const Flex = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+`;
+
+const GameHeading = styled.h1`
+  font-weight: ${typography.fontWeights.black};
+  font-family: ${typography.fonts.primary};
+  font-size: 16px;
+  color: ${colors.white};
+  user-select: none;
+`;
+
+const GameCard = ({ children, gridTemplateRows, game, onGameDelete }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalData, setModalData] = useState({});
+
+    return (
+        <React.Fragment>
+            <StyledCard
+                style={{
+                    gridTemplateRows: gridTemplateRows,
+                }}
+                onClick={() => {
+                    setModalData(game);
+                    setIsModalOpen(true);
+                }}
+            >
+                <img
+                    src="/prechess.jpg"
+                    width="100%"
+                    height="auto"
+                    margin="0"
+                    alt="prechess"
+                    style={{
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    opacity: "0.85",
+                    }}
+                />
+                <Flex
+                    style={{
+                    justifyContent: "flex-start",
+                    width: "100%",
+                    marginLeft: "4%",
+                    }}
+                >
+                    <GameHeading style={{ marginRight: "5%", lineHeight: "28px", textAlign: "left" }}> 
+                    🎯 {game.name}<br/>
+                    ⌛ {new Date(game.updatedAt).toLocaleString("en-US", { dateStyle: "long"})}
+                    </GameHeading>
+                </Flex>
+            </StyledCard>
+            <GameModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                title={"My Creations 📚"}
+                onDelete={onGameDelete}
+                gameData={modalData}
+            />
+        </React.Fragment>
+    );
 };
 
 export { GameCard };
