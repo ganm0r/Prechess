81d1f21708bf6a1c7a8fa01af042c9731576e9c7
ganm0r <gndhrvmv@gmail.com>
2022-06-22 16:03:29 +0530

    :children_crossing: Add toast for successful PGN copy

--- frontend/src/components/GameModal.jsx
+++ frontend/src/components/GameModal.jsx
@@ -5,6 +5,7 @@ import typography from "../theme/typography";
 
 import { GrClose } from "react-icons/gr";
 import { Button } from "./Button";
+import { toast } from "react-toastify";
 
 const Overlay = styled.div`
   position: fixed;
